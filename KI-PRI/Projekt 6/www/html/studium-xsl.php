b3dc2589d9e3d1f60266457e2349d4947a6298d6
Jan Burle <jan.burle@pm.me>
2024-03-26 18:05:07 +0100

    .

--- KI-PRI/Projekt 6/www/html/studium-xsl.php
+++ KI-PRI/Projekt 6/www/html/studium-xsl.php
@@ -3,8 +3,8 @@ $xml = new DOMDocument;
 $xml->load('../xml/studium.xml');
 
 $xsl = new DOMDocument;
-$xsl->load('../xml/studium-1-a.xsl');
-// $xsl->load('../xml/studium-1-b.xsl');
+// $xsl->load('../xml/studium-1.xsl');
+$xsl->load('../xml/studium-2.xsl');
 // $xsl->load('../xml/studium-1-c.xsl');
 // $xsl->load('../xml/studium-1-d.xsl');
 
