c9ab993a60879afa9a44f59a85cb513097511ee7
Jan Burle <jan.burle@pm.me>
2024-04-28 15:58:10 +0200

    .

--- Projekty/Projekt - fullstack - objektově/www/html/serve/getDrink.php
+++
@@ -1,8 +0,0 @@
-<?php // přečti a odešli XML soubor s receptem
-
-$drink = @$_GET['drink'];
-$file = "/var/mixolog/drinks/$drink.xml";
-
-header("Content-type: text/xml;");
-if (file_exists($file))
-    readfile($file);
