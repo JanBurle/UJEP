40605cb415d8274bf80b55157cff9e333997f38d
Jan Burle <jan.burle@pm.me>
2025-10-09 17:01:58 +0200

    .

--- KI-RDBS/projekt/init/1 - schema.sql
+++ KI-RDBS/projekt/init/1 - schema.sql
@@ -1,6 +1,34 @@
--- create data model
+-- data model
 
-create table hello (
-    id serial primary key,
-    msg text not null
+create table continent (
+  id   integer      primary key,
+  name varchar(100) not null unique
+);
+
+create table country (
+  code         char(2)      primary key,
+  id_continent integer      references continent(id),
+  name         varchar(100) not null unique
+);
+
+create table place (
+  id           serial       primary key,
+  name         varchar(100) not null,
+  code_country char(2)      references country(code),
+  latitude     decimal(8,5) not null,
+  longitude    decimal(8,5) not null,
+  elevation    int
+);
+
+-- daily weather data
+create table weather (
+    id       serial primary key,
+    id_place int    references place(id),
+    date     date   not null,
+
+    temp_lo  decimal(4,1),  -- daily min temp (°C)
+    temp_hi  decimal(4,1),  -- daily max temp (°C)
+    precip   decimal(6,2),  -- precipitation (mm)
+
+    unique(id_place, date)
 );
--- KI-RDBS/projekt/init/2 - data.sql
+++ KI-RDBS/projekt/init/2 - data.sql
@@ -1,13 +1,2 @@
--- insert test data
+-- test data
 
-insert into hello (msg) values
-  ('Hello, World!'),
-  ('Ahoj, světe!'),
-  ('Hallo, Welt!'),
-  ('Bonjour, le monde!'),
-  ('Hola, mundo!'),
-  ('Ciao, mondo!'),
-  ('Hej, världen!'),
-  ('Olá, mundo!'),
-  ('Привет, мир!'),
-  ('こんにちは、世界！');
